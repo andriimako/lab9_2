d0719727308b66321d82155c6d9632a6d96d9229
s17386 <61780276+s17386@users.noreply.github.com>
2020-05-13 22:17:00 +0200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    public void sleep (){
+        System.out.println("sleeps");
+    }
+    public void makeNoise(){
+        System.out.println("makes noises");
+    }
+    public void roam(){
+        System.out.println("roams");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[]args){
+        Cat losharique = new Cat();
+        Dog sharique = new Dog();
+        Lion lion4ik = new Lion();
+        Wolf volk = new Wolf();
+
+        losharique.makeNoise();
+        sharique.makeNoise();
+        lion4ik.makeNoise();
+        volk.makeNoise();
+        losharique.roam();
+        lion4ik.roam();
+        sharique.sleep();
+
+
+    }
+}
